0b157eabfafff774aad3bec293ed8b0e6ce9c871
paclaraujo <paclaraujo@gmail.com>
2020-06-12 13:30:57 -0300

    boilerplate

--- public/index.js
+++ public/index.js
@@ -1,4 +1,1 @@
-// Este é o ponto de entrada de sua aplicação
-import { home } from './pages/home/main.js';
 
-document.querySelector('#root').appendChild(home());
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,1 @@
-// importamos as funções que iremos testar
-import { greeting } from '../public/pages/home/data.js';
 
-describe('Greeting', () => {
-  it('Deveria retornar "Oi Maria! Que bom ver você aqui!" quando passado "Maria" como parâmetros', () => {
-    const message = 'Oi Maria! Que bom ver você aqui!';
-    expect(greeting('Maria')).toEqual(message);
-  });
-});
