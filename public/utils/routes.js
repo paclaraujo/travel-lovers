9120b9f4d3b0b7296c6845f58939873cfa94c661
paclaraujo <paclaraujo@gmail.com>
2020-06-13 02:54:16 -0300

    add routes file

---
+++ public/utils/routes.js
@@ -0,0 +1,7 @@
+import { loginTemplate } from './pages/login/loginView.js';
+import { registerTemplate } from './pages/register/registerView.js';
+
+export const routes = {
+  login: loginTemplate(),
+  register: registerTemplate()
+}
