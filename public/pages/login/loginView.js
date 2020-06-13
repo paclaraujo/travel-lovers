1a246f6362d1fa23e3db1f43ca9969178cedb5f9
paclaraujo <paclaraujo@gmail.com>
2020-06-12 23:06:12 -0300

    add login template

---
+++ public/pages/login/loginView.js
@@ -0,0 +1,27 @@
+export const loginTemplate = () => {
+  const loginContainer = document.createElement('div');
+
+  loginContainer.innerHTML = `
+    <main class='login-container'>
+      <div class='login container-shadow'>
+        <div class='bg-white login-box'>
+          <h1 class='title'>TraveLovers</h1>
+          <form class='login-form'>
+            <label>Email:</label>
+            <input class='login-input' placeholder='example@example.com' id='email' type='email'>
+            <label >Password:</label>
+            <input class='login-input' placeholder='***********' id='password' type='password'>
+            <span class='error-message'></span>
+          </form>
+          <div class='btn-box'>
+            <button class='btn container-shadow' id='login-btn'>Login</button>
+            <p class='text'>Log in with google</p>
+            <button class='google-btn fab fa-google' id='login-google'></button>
+            <p class='text'>Don't have an account? <a class='register-anchor' href='#register'>Register</a></p>
+          </div>
+        </div>
+      </div>
+    </main>`;
+
+  return loginContainer;
+};
