76955035489d32c87e46554927c705064c558a8b
paclaraujo <paclaraujo@gmail.com>
2020-06-13 02:39:02 -0300

    add register error messages

--- public/utils/error.js
+++ public/utils/error.js
@@ -4,5 +4,11 @@ export const errorMessages = {
     'auth/invalid-action-code': 'Código inválido',
     'auth/user-disabled': 'Usuário desabilitado',
     'auth/user-not-found': 'Usuário não encontrado'
+  },
+  register: {
+    'auth/email-already-in-use': 'Email já cadastrado',
+    'auth/invalid-email': 'Email inválido',
+    'auth/operation-not-allowed': 'Ação não autorizada',
+    'auth/weak-password': 'Senha fraca'
   }
 };
