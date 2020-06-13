d8ba0af941ae16f15601fa054641a43377a9c69a
paclaraujo <paclaraujo@gmail.com>
2020-06-13 00:53:08 -0300

    add error messages object

---
+++ public/utils/error.js
@@ -0,0 +1,8 @@
+export const errorMessages = {
+  auth: {
+    'auth/expired-action-code': 'Código expirado',
+    'auth/invalid-action-code': 'Código inválido',
+    'auth/user-disabled': 'Usuário desabilitado',
+    'auth/user-not-found': 'Usuário não encontrado'
+  }
+};
