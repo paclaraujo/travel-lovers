80f84ab3e63327dfcc7a6a0763833ca3ee3d9493
paclaraujo <paclaraujo@gmail.com>
2020-06-13 22:18:54 -0300

    add initDb function

--- public/utils/commonScripts.js
+++ public/utils/commonScripts.js
@@ -1,15 +1,12 @@
-import { errorMessages } from './error.js';
-
-export const showErrorMessage = (error, type) => {
-  const errorMessage = document.querySelector('#error-message');
-  errorMessage.textContent = errorMessages[type][error]; 
-}
+import { printPosts } from '../pages/feed/posts/postsEvents.js'; 
+import { getUser } from '../pages/feed/feedFunctions.js'
 
 export const addUserDoc = (user) => {
   firebase
     .firestore()
     .collection('users')
     .doc(user.uid).get().then((doc) => {
+      location.hash = 'feed';
       if (!doc.exists) {
         firebase.firestore().collection('users').doc(user.uid).set({
           name: user.displayName,
@@ -19,3 +16,18 @@ export const addUserDoc = (user) => {
       };
     })
 };
+
+export const initDb = () => {
+  firebase.firestore().collection("posts").orderBy('timestamp', 'desc')
+    .onSnapshot((querySnapshot) => {
+      const { user_uid } = getUser();
+      const allPosts = [];
+      querySnapshot.forEach(function(doc) {
+        if(doc.data().user_uid === user_uid || doc.data().privacy === "Public"){
+          allPosts.push({...doc.data(), id: doc.id});
+        } 
+      });
+      printPosts(allPosts);  
+    }
+  );
+}
