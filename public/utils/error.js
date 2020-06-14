ce4aa5bfcc5d4cb0f74b7dd7b1f653ff87741a99
paclaraujo <paclaraujo@gmail.com>
2020-06-13 22:18:03 -0300

    add showErrorMessage function

--- public/utils/error.js
+++ public/utils/error.js
@@ -1,14 +1,4 @@
-export const errorMessages = {
-  auth: {
-    'auth/expired-action-code': 'Código expirado',
-    'auth/invalid-action-code': 'Código inválido',
-    'auth/user-disabled': 'Usuário desabilitado',
-    'auth/user-not-found': 'Usuário não encontrado'
-  },
-  register: {
-    'auth/email-already-in-use': 'Email já cadastrado',
-    'auth/invalid-email': 'Email inválido',
-    'auth/operation-not-allowed': 'Ação não autorizada',
-    'auth/weak-password': 'Senha fraca'
-  }
-};
+export const showErrorMessage = (error) => {
+  const errorMessage = document.querySelector('#error-message');
+  errorMessage.textContent = error.message; 
+}
