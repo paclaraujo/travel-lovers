f10fad0814108f31a22b9811fd1a737aa2587e51
paclaraujo <paclaraujo@gmail.com>
2020-06-13 22:17:12 -0300

    update routes file

--- public/utils/routes.js
+++ public/utils/routes.js
@@ -1,7 +1,9 @@
 import { loginTemplate } from '../pages/login/loginView.js';
 import { registerTemplate } from '../pages/register/registerView.js';
+import { feedTemplate } from '../pages/feed/feedView.js';
 
 export const routes = {
-  login: loginTemplate(),
-  register: registerTemplate()
+  login: loginTemplate,
+  register: registerTemplate,
+  feed: feedTemplate
 }
