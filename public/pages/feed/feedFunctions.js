377612f7908b834b7e584258c47f8bafef0e869f
paclaraujo <paclaraujo@gmail.com>
2020-06-13 22:23:40 -0300

    add firebase feed functions

---
+++ public/pages/feed/feedFunctions.js
@@ -0,0 +1,38 @@
+export const getUser = () => {
+  const { uid, displayName } = firebase.auth().currentUser;
+  return {
+    user_uid: uid,
+    name: displayName
+  }
+}
+
+export const logout = () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => (location.hash = 'login'))
+    .catch(error => (error));
+}
+
+const dateFormat = () => {
+  const options = {year: 'numeric', month: "long", day: "numeric", hour: 'numeric', minute: "numeric"};
+  return new Intl.DateTimeFormat('en', options).format(new Date())
+} 
+
+export const addPostDb = (post) => {
+  firebase
+    .firestore()
+    .collection("posts")
+    .doc()
+    .set({
+      ...post,
+      ...getUser(),
+      likes: 0,
+      comments: [],
+      timestamp: new Date().getTime(),
+      date: dateFormat()
+    }
+  )
+}
+
+
